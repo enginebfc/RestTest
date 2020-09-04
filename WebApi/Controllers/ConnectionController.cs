94ea1badc8c2c1a22bf775faa25233a78ff3804b
liaudanski.d <liaudanski.d@nakukop.com>
2020-09-04 14:56:52 +0200

    initial commit

---
+++ WebApi/Controllers/ConnectionController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConnectionController
+    {
+        public ConnectionController()
+        {
+
+        }
+
+        [HttpGet]
+        public HiModel GetHi(int id)
+        {
+            return new HiModel { Id = id, Name = "Some name" };
+        }
+
+        [HttpPost]
+        public int Create(HiModel request)
+        {
+            return 1;
+        }
+    }
+}
---
+++ WebApi/Validators/HiModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using WebApi.Models;
+
+namespace WebApi.Validators
+{
+    public class HiModelValidator : AbstractValidator<HiModel>
+    {
+        public HiModelValidator()
+        {
+            RuleFor(x => x.Id).GreaterThan(0);
+            RuleFor(x => x.Name).NotEmpty();
+        }   
+    }
+}
