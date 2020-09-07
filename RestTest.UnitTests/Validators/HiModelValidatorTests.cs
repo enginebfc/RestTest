02c56e1e767b9714084d946751751279d68a153c
Dzmitry Liaudanski <dliaudanski@techona.com>
2020-09-07 21:50:39 +0200

    new proj

---
+++ RestTest.UnitTests/Validators/HiModelValidatorTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation.TestHelper;
+using WebApi.Models;
+using WebApi.Validators;
+using Xunit;
+
+namespace RestTest.UnitTests.Validators
+{
+    public class HiModelValidatorTests
+    {
+        public HiModelValidatorTests()
+        {
+            Validator = new HiModelValidator();
+        }
+
+        public HiModelValidator Validator { get; set; }
+
+        [Fact]
+        public void Negative_Id_Returns_Error()
+        {
+            HiModel model = new HiModel {Id = -1, Name = "Ok"};
+            TestValidationResult<HiModel> result = Validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(mode => mode.Id);
+        }
+
+        [Fact]
+        public void Positive_Id_Returns_OK()
+        {
+            HiModel model = new HiModel {Id = 1, Name = "Ok"};
+            TestValidationResult<HiModel> result = Validator.TestValidate(model);
+            result.ShouldNotHaveValidationErrorFor(mod => mod.Id);
+        }
+    }
+}
---
+++ WebApi/Interfaces/IHiRepository.cs
@@ -0,0 +1,10 @@
+﻿using WebApi.Models;
+
+namespace RestTest.Interfaces
+{
+    public interface IHiRepository
+    {
+        HiModel Get(int id);
+        int Add(HiModel model);
+    }
+}
