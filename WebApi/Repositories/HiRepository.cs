8caedbf49405c6ba04d48d032870bc79f90f8bd0
Dzmitry Liaudanski <dliaudanski@techona.com>
2020-09-07 21:49:54 +0200

    adding new project

---
+++ WebApi/Repositories/HiRepository.cs
@@ -0,0 +1,18 @@
+﻿using RestTest.Interfaces;
+using WebApi.Models;
+
+namespace RestTest.Repositories
+{
+    public class HiRepository : IHiRepository
+    {
+        public HiModel Get(int id)
+        {
+            return new HiModel {Id = 1, Name = "1 model"};
+        }
+
+        public int Add(HiModel model)
+        {
+            return 1;
+        }
+    }
+}
