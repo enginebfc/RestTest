dfc13e0104e9e235c1717c6d73318da847f0ffe7
liaudanski.d <liaudanski.d@nakukop.com>
2020-09-10 14:42:57 +0200

    bit more tests

---
+++ RestTest.UnitTests/Controllers/HiControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RestTest.Controllers;
+using RestTest.Interfaces;
+using WebApi.Models;
+using Xunit;
+
+namespace RestTest.UnitTests.Controllers
+{
+    public class HiControllerTests
+    {
+        public Mock<IHiRepository> Stub { get; set; }
+        HiController Controller { get; set; }
+
+        public HiControllerTests()
+        {
+            Stub = new Mock<IHiRepository>();
+            Stub.Setup(x => x.Get(It.IsAny<int>())).Returns(new WebApi.Models.HiModel());
+            Controller = new HiController(Stub.Object);
+        }
+
+        [Fact]
+        public void Get_Returns_Ok()
+        {
+            var resp = Controller.Get(1);
+            Stub.Verify(x => x.Get(It.IsAny<int>()), Times.Once);
+            Assert.IsType<ActionResult<HiModel>>(resp);
+        }
+
+
+    }
+}
